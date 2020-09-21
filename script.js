5c9f45bd9f248d714249d0724eb629180044df37
gokul <ygokul1702@gmail.com>
2020-09-21 09:02:22 +0530

    added input feature

--- script.js
+++ script.js
@@ -1,23 +1,51 @@
+let quest = document.getElementById('quest');
+let ans = document.getElementById('answer');
+let inputitem = document.getElementById('inputitem');
+let enter = document.getElementById('enter')
+let num1 = document.getElementById('num1')
+let num2 = document.getElementById('num2')
+
 let a=0,b=0;
-        let quest = document.getElementById('quest');
-        let ans = document.getElementById('answer');
-        let inputitem = document.getElementById('inputitem');
-        let button = document.getElementById('enter-btn');
+let num1_digit = 2, num2_digit = 2;
+let num1_min, num1_max, num2_min, num2_max;
 
-        button.addEventListener('click',()=>{
-            console.log(inputitem.value)
-        })
+window.onload = () => {
+    minmax(num1_digit,1)
+    minmax(num2_digit,2)
+    getQuest()
+}
 
-        function getRndInteger(min, max) {
-            return Math.floor(Math.random() * (max - min) ) + min;
-        }
-        function getQuest(){
-            a = getRndInteger(10,99);
-            b = getRndInteger(10,99);
-            quest.innerHTML = `${a} x ${b} :`;
-            ans.innerHTML = "";
-        }
+enter.addEventListener('click',(e) => {
+    e.preventDefault()
+    num1_digit = num1.value
+    minmax(num1_digit,1)
+    num2_digit = num2.value
+    minmax(num2_digit,2)
+    getQuest()
+})
 
-        function answer(){
-            ans.innerHTML = a*b;
-        }
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+}
+function getQuest(){
+    a = getRndInteger(num1_min,num1_max);
+    b = getRndInteger(num2_min,num2_max);
+    quest.innerHTML = `${a} x ${b} =`;
+    ans.innerHTML = "";
+}
+
+function answer(){
+    ans.innerHTML = a*b;
+}
+
+const minmax = (digit,num) => {
+    if(num == 1){
+        num1_min = 10 ** (digit-1)
+        num1_max = (10 ** digit) - 1
+    }else{
+        num2_min = 10 ** (digit-1)
+        num2_max = (10 ** digit) - 1
+    }
+}
+
